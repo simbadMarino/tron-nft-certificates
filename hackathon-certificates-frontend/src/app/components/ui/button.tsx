8229eb0dfe15df0f5141107988263fd954b5c3ad
adeelch9 <adeel.chaudhary@tron.network>
2025-01-17 10:36:26 -0500

    button color fix

--- hackathon-certificates-frontend/src/app/components/ui/button.tsx
+++ hackathon-certificates-frontend/src/app/components/ui/button.tsx
@@ -6,7 +6,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "ext-primary-foreground",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
