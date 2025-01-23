8a39aedffcad9adb00839a1df2acda4ed5d74d3e
adeelch9 <adeel.chaudhary@tron.network>
2025-01-23 10:06:47 -0500

    code improvements

--- backend/contracts/TronNFTCollection.sol
+++ backend/contracts/TronNFTCollection.sol
@@ -10,6 +10,7 @@ contract TronNFTCollection is ERC721, ERC721URIStorage, Ownable, ERC721Burnable,
     mapping(uint256 => string) private _certificateURIs; // Mapping for storing 
     mapping(address => bool) private _allowedMinters;
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    uint256 private _tokenIdsCounter;
 
     constructor(address _owner) ERC721("Tron NFT Collection", "TNC") ERC721URIStorage() Ownable(msg.sender) {
         _grantRole(ADMIN_ROLE, msg.sender); // Assign the deployer as the admin
@@ -20,7 +21,7 @@ contract TronNFTCollection is ERC721, ERC721URIStorage, Ownable, ERC721Burnable,
         _;
     }
 
-    function safeMint(address to, uint256 tokenId, string memory uri) external {
+    function safeMint(address to, uint256 tokenId, string memory uri) external onlyAdmin {
         require(_allowedMinters[msg.sender], "Not allowed to mint");
         require(bytes(uri).length > 0, "Invalid URI");
         _safeMint(to, tokenId);
@@ -68,10 +69,13 @@ contract TronNFTCollection is ERC721, ERC721URIStorage, Ownable, ERC721Burnable,
         return _allowedMinters[minter];
     }
 
-    function uploadCertificateURI(uint256 tokenId, string memory uri) external onlyAdmin {
+    function uploadCertificateURI(string memory uri) external onlyAdmin {
         //check if the URI is valid
         require(bytes(uri).length > 0, "Invalid URI");
+        _tokenIdsCounter++;
+        uint256 tokenId = _tokenIdsCounter;
         //check if the token ID exists
         _certificateURIs[tokenId] = uri;
     }
+
 }
