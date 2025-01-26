d28834adf835e888f4a03e470e96111aac1a4103
adeelch9 <adeel.chaudhary@tron.network>
2025-01-26 12:23:39 -0500

    button text color update

--- hackathon-certificates-frontend/src/app/components/ui/button.tsx
+++ hackathon-certificates-frontend/src/app/components/ui/button.tsx
@@ -2,11 +2,11 @@ import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
 const buttonVariants = cva(
-  "text-black inline-flex items-center justify-center rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center rounded-full text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
-        default: "text-primary-foreground",
+        default: "text-black",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
